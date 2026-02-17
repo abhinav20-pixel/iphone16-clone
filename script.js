4426f85e5186f7f182f3d756896b24a738ec2f52
Abhinav Kumar <abhinavgraphics36@gmail.com>
2026-02-17 20:24:50 +0530

    Initial commit - iPhone 16 clone

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector(".buy-now").addEventListener("click", () => {
+    alert("Proceeding to checkout...");
+});
+
+document.querySelector(".add-cart").addEventListener("click", () => {
+    alert("Item added to cart!");
+});
